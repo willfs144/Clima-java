3e176918fc0e557a7e01a509cb43613f2ca9c118
William F. Suarez B <willfs144@gmail.com>
2020-09-29 22:38:23 -0500

    Mostrar dato variable

--- src/persistencia/Leer.java
+++ src/persistencia/Leer.java
@@ -1,35 +1,60 @@
 package persistencia;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 
+import ucar.ma2.InvalidRangeException;
+import ucar.nc2.NCdumpW;
 import ucar.nc2.NetcdfFile;
+import ucar.nc2.Variable;
 
 
 
 public class Leer {
-	String filename = "D:\\William\\Proyectos_realizados\\Java\\POO\\Clima\\src\\recursos\\outN.1";
+	String nombreArchivo = "D:\\William\\Proyectos_realizados\\Java\\POO\\Clima\\src\\recursos\\outN.4";
+	String nombreVariable1 = "radialElev";
+	String nombreVariable2 = "siteLat";
 	
-	public Leer() {
-		
+	public Leer() {		
 		
 		  NetcdfFile ncfile = null;
 		  try {
-		    ncfile = NetcdfFile.open(filename);
+		    ncfile = NetcdfFile.open(nombreArchivo);
 		    process( ncfile);
+		    
 		  } catch (IOException ioe) {
-		    System.out.println("trying to open " + filename.toString()+"  "+ioe);
+		    System.out.println("trying to open " + nombreArchivo.toString()+"  "+ioe);
 		  } finally { 
 		    if (null != ncfile) try {
 		      ncfile.close();
 		    } catch (IOException ioe) {
-		     System.out.println("trying to close " + filename.toString()+"  "+ioe);
+		     System.out.println("trying to close " + nombreArchivo.toString()+"  "+ioe);
 		    }
 		  }
 		
 	}
 
 	public void process(NetcdfFile ncfile) {
-		System.out.println(ncfile.toString());	
+		//System.out.println(ncfile.toString());
+		mostrarDatosVariable(ncfile);
+	}
+
+	private void mostrarDatosVariable(NetcdfFile ncfile) {
+		
+		Variable v = ncfile.findVariable(nombreVariable1);
+		  if (null == v) {
+			  System.out.println("nulo");
+			  return;
+		  }
+		 try {
+		 ucar.ma2.Array data = v.read();
+		 System.out.println("Resultado: "+data.toString());
+		  
+		 } catch (IOException ioe) {
+			 System.out.println("trying to read " + nombreVariable1+" "+ioe);
+
+		  }
+		
 	}
 
 	public static void main(String[] args) {
